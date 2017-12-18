94eb6b690382fdd401607e7b2023acde45ec2e49
yudr31 <yuderen6312@163.com>
2017-12-18 15:41:48 +0800

    更新

---
+++ sql.sh
@@ -0,0 +1,8 @@
+mysql -uroot -p123456 nnk_code -e "source /opt/genCode/db.sql"
+
+mysql -uroot -p123456 nnk_code -e "source /opt/genCode/fragmentTemplate.sql"
+
+mysql -uroot -p123456 nnk_code -e "source /opt/genCode/modelConfig.sql"
+
+mysql -uroot -p123456 nnk_code -e "source /opt/genCode/demo.sql"
+
